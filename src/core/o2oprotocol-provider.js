e2ea6526af0734e745901fd8801a67fc335cb7e9
Anh Le Hoang <lehoanganh25991@gmail.com>
2018-05-16 15:13:06 +0700

    Update init o2o

--- src/core/o2oprotocol-provider.js
+++ src/core/o2oprotocol-provider.js
@@ -1,23 +1,29 @@
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
-import {O2OProtocol} from 'o2oprotocol';
+// import {O2OProtocol} from 'o2oprotocol';
+import O2OProtocol from 'o2oprotocol';
 
 class O2OProtocolProvider extends Component {
   constructor(props) {
     super(props);
 
     const {web3} = window;
-    const ipfsConfig = {
-      ipfsDomain: '52.198.14.106',
-      ipfsApiPort: '5001',
-      ipfsGatewayPort: '8088',
-      ipfsProtocol: 'http'
-    };
-    this.initO2O({web3, ipfsConfig});
+    const defaultIpfsDomain = process.env.REACT_APP_IPFS_SERVER_IP || "localhost"
+    const defaultIpfsApiPort = process.env.REACT_APP_IPFS_API_PORT || "5001"
+    const defaultIpfsGatewayPort = process.env.REACT_APP_IPFS_GATEWAY_PORT || "8081"
+    const defaultIpfsGatewayProtocol = process.env.REACT_APP_IPFS_GATEWAY_PROTOCOL || "http"
+    
+    this.initO2O({web3, 
+      ipfsDomain: defaultIpfsDomain,
+      ipfsApiPort: defaultIpfsApiPort,
+      ipfsGatewayPort: defaultIpfsGatewayPort,
+      ipfsGatewayProtocol: defaultIpfsGatewayProtocol,
+    });
   }
 
   initO2O(options) {
     this.o2oprotocol = new O2OProtocol(options);
+    window.o2o = this.o2oprotocol
     console.log('o2oprotocol was initialized!');
   }
 
